1a735af06c9505c1725a0ce9aa597b89cabfa9db
Nick Bristow <Nick.Bristow@gsa.gov>
2015-06-17 13:34:13 -0400

    added secret key to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.assets.initialize_on_precompile = false
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
